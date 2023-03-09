d4f967c3812e3ab27eaf0f908c03bb79a3b7c6ab
小小孩子们 <2595557847@qq.com>
2023-03-09 22:24:42 +0800

    更新云字体库，全部字体均为免费可商用字体

--- static/css/css.php
+++ static/css/css.php
@@ -8,60 +8,8 @@ $Customize = $_GET['Customize'] ?? '';
 
 if ($Customize !== '') {
   $fileName = $Customize;
-} else {
-  switch ($_GET['type']) {
-    case '2':
-      $fileName = $_GET['path'] . '/方正像素.ttf';
-      break;
-
-    case '3':
-      $fileName = $_GET['path'] . '/方正兰亭细黑.ttf';
-      break;
-
-    case '4':
-      $fileName = $_GET['path'] . '/方正北魏楷书简体.ttf';
-      break;
-
-    case '5':
-      $fileName = $_GET['path'] . '/方正细俊黑简体.ttf';
-      break;
-
-    case '6':
-      $fileName = $_GET['path'] . '/汉仪中楷简.ttf';
-      break;
-
-    case '7':
-      $fileName = $_GET['path'] . '/汉仪劲楷简.ttf';
-      break;
-
-    case '8':
-      $fileName = $_GET['path'] . '/汉仪粗仿宋简.ttf';
-      break;
-
-    case '9':
-      $fileName = $_GET['path'] . '/造字工房典黑超细体.ttf';
-      break;
-
-    case '10':
-      $fileName = $_GET['path'] . '/造字工房尚黑常规体.ttf';
-      break;
-
-    case '11':
-      $fileName = $_GET['path'] . '/造字工房悦圆.otf';
-      break;
-
-    case '12':
-      $fileName = $_GET['path'] . '/潮玩嬉戏体.ttf';
-      break;
-
-    case '13':
-      $fileName = $_GET['path'] . '/快乐灵影体.ttf';
-      break;
-
-    default:
-      echo "document.write(\"FontLibs插件错误\")";
-      exit;
-  }
+} else if ($_GET['type'] !== '0') {
+  $fileName = $_GET['path'] . $_GET['type'] . ".ttf";
 }
 
 // 输出css
